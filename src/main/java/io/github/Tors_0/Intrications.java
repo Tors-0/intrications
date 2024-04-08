2c69f3b9066981c74a42b2e593ace4b497faed21
rae <raej0000@gmail.com>
2024-04-07 23:33:30 -0700

    mod init

---
+++ src/main/java/io/github/Tors_0/Intrications.java
@@ -0,0 +1,11 @@
+package io.github.Tors_0;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class Intrications implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
