6f57b79bf979b21af7613ba9f143cf8b55f06c38
rae <raej0000@gmail.com>
2024-04-11 01:40:10 -0700

    polish tp staff behaviour and use damage indicator to show charge

--- src/main/java/io/github/Tors_0/intrications/item/TeleportStaffItem.java
+++ src/main/java/io/github/Tors_0/intrications/item/TeleportStaffItem.java
@@ -1,73 +1,123 @@
 package io.github.Tors_0.intrications.item;
 
-import io.github.Tors_0.intrications.Intrications;
 import io.github.Tors_0.intrications.util.ItemModified;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.Vanishable;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
-public class TeleportStaffItem extends Item {
+public class TeleportStaffItem extends Item implements Vanishable {
+	public static final ItemStack FUEL_ITEM = new ItemStack(Items.ENDER_PEARL);
+	public static final int MAX_DISTANCE = 50;
 
 	public TeleportStaffItem(Settings settings) {
-		super(settings);
+		super(settings.maxDamage(MAX_DISTANCE + 1));
+	}
+
+	public int getMaxUseTime(ItemStack stack) {
+		return 120;
+	}
+
+	public UseAction getUseAction(ItemStack stack) {
+		return UseAction.BOW;
 	}
 
 	@Override
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 		if (!world.isClient()) {
-			double maxDistance = 20d;
-			float tickDelta = 1f;
-			Vec3d vec3d = user.getCameraPosVec(tickDelta);
-			Vec3d vec3d5 = user.getRotationVec(tickDelta);
-			Vec3d vec3d6 = vec3d.add(vec3d5.x * maxDistance, vec3d5.y * maxDistance, vec3d5.z * maxDistance);
-			HitResult blockResult = world
-				.raycast(new RaycastContext(
-					vec3d, vec3d6, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, user
-				));
-			Vec3d startPoint = user.getPos().add(0.0, 1.6F, 0.0);
-			BlockHitResult hitResult = ItemModified.raycast(
-				world,
-				user,
-				RaycastContext.ShapeType.COLLIDER,
-				RaycastContext.FluidHandling.NONE,
-				maxDistance
-			);
-			// coordinate handling to avoid teleporting into blocks
-
-			BlockPos pos = new BlockPos(hitResult.getPos());
-			//pos = pos.offset(hitResult.getSide(), 1);
-
-			user.teleport(pos.getX() + .5, pos.getY(), pos.getZ() + .5, true);
-
-			Vec3d finalPos = new Vec3d(pos.getX(), pos.getY(), pos.getZ());
-
-			Intrications.LOGGER.info("offset: " + finalPos.subtract(user.getPos()));
-			// particles along path & arrival sound
-			Vec3d vec3d2 = finalPos.subtract(startPoint);
-			Vec3d vec3d3 = vec3d2.normalize();
-			for (int i = 1; i < MathHelper.floor(vec3d2.length()) + 1; ++i) {
-				Vec3d vec3d4 = startPoint.add(vec3d3.multiply(i));
-				((ServerWorld) world).spawnParticles(ParticleTypes.FIREWORK, vec3d4.x, vec3d4.y, vec3d4.z, 1, 0.0, 0.0, 0.0, 0.0);
+			ItemStack itemStack = user.getStackInHand(hand);
+			if (!user.isCreative() && !user.getInventory().contains(FUEL_ITEM)) {
+				return TypedActionResult.fail(itemStack);
+			} else {
+				user.setCurrentHand(hand);
+				return TypedActionResult.consume(itemStack);
 			}
-			user.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 3.0F, 1.0F);
-			return TypedActionResult.success(user.getStackInHand(hand));
 		} else {
-			return TypedActionResult.fail(user.getStackInHand(hand));
+			return TypedActionResult.pass(user.getStackInHand(hand));
+		}
+	}
+
+	public float getDrawPercentage(ItemStack stack, int remainingUseTicks) {
+		return (float) (getMaxUseTime(stack) - remainingUseTicks) / getMaxUseTime(stack);
+	}
+
+	@Override
+	public boolean isItemBarVisible(ItemStack stack) {
+		return super.isItemBarVisible(stack);
+	}
+
+	@Override
+	public int getItemBarColor(ItemStack stack) {
+		return super.getItemBarColor(stack);
+	}
+
+	@Override
+	public boolean isDamageable() {
+		return true;
+	}
+
+	@Override
+	public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+		stack.setDamage(stack.getMaxDamage() - Math.max(1,(int) (MAX_DISTANCE * getDrawPercentage(stack, remainingUseTicks))));
+	}
+
+	@Override
+	public int getItemBarStep(ItemStack stack) {
+		return super.getItemBarStep(stack);
+	}
+
+	@Override
+	public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+		if (!world.isClient() && user instanceof PlayerEntity player) {
+			if (player.getInventory().contains(FUEL_ITEM) || player.isCreative()) {
+				ItemStack itemStack = player.getInventory().getStack(player.getInventory().getSlotWithStack(FUEL_ITEM));
+				double maxDistance = MAX_DISTANCE * getDrawPercentage(stack, remainingUseTicks);
+				Vec3d startPoint = player.getPos().add(0.0, 1.6F, 0.0);
+				BlockHitResult hitResult = ItemModified.raycast(
+					world,
+					player,
+					RaycastContext.ShapeType.COLLIDER,
+					RaycastContext.FluidHandling.NONE,
+					maxDistance
+				);
+
+				BlockPos pos = new BlockPos(hitResult.getPos());
+
+				// coordinate handling to avoid teleporting into blocks
+				player.teleport(pos.getX() + .5, pos.getY(), pos.getZ() + .5, true);
+
+				// particles & arrival sound
+				for (int i = 1; i < 15; ++i) {
+                    ((ServerWorld) world).spawnParticles(ParticleTypes.END_ROD, startPoint.x, startPoint.y, startPoint.z, 1, 0, 0, 0, Math.random() * .5);
+				}
+				player.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 3.0F, 1.0F);
+
+				// if player isn't in creative, remove an ender pearl
+				if (!player.isCreative() && !itemStack.isEmpty()) {
+					itemStack.decrement(1);
+					if (itemStack.isEmpty()) {
+						player.getInventory().removeOne(itemStack);
+					}
+				}
+
+				stack.setDamage(0);
+				player.incrementStat(Stats.USED.getOrCreateStat(this));
+			}
 		}
 	}
 }
