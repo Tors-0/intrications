6fd3eed63f3453893cba544dbbe417052c4e3081
rae <raej0000@gmail.com>
2024-04-11 01:40:35 -0700

    here if needed, else mark for deletion

---
+++ src/main/java/io/github/Tors_0/intrications/command/CustomTitleCommand.java
@@ -0,0 +1,17 @@
+package io.github.Tors_0.intrications.command;
+
+import net.minecraft.network.Packet;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
+
+import java.util.function.Function;
+
+public class CustomTitleCommand {
+	public static void executeTitle(ServerCommandSource source, ServerPlayerEntity target, Text title, String titleType, Function<Text, Packet<?>> constructor) {
+		try {
+			target.networkHandler.sendPacket((Packet) constructor.apply(Texts.parse(source, title, target, 0)));
+		} catch (Exception ignored) {}
+	}
+}
