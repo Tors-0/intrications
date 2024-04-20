2fe5405f782ea1b92a3cb35429d0b2f5f467e720
rae <raej0000@gmail.com>
2024-04-20 15:52:51 -0700

    fix mod crashing dedicated servers

---
+++ src/main/java/io/github/Tors_0/intrications/IntricationsClient.java
@@ -0,0 +1,36 @@
+package io.github.Tors_0.intrications;
+
+import io.github.Tors_0.intrications.entity.render.MagicMissileEntityRenderer;
+import io.github.Tors_0.intrications.item.TeleportStaffItem;
+import io.github.Tors_0.intrications.registry.IntricationsEntities;
+import io.github.Tors_0.intrications.registry.IntricationsItems;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.util.Identifier;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.minecraft.ClientOnly;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+@ClientOnly
+public class IntricationsClient implements ClientModInitializer {
+	static {
+		// create model predicate providers
+		ModelPredicateProviderRegistry.register(
+			IntricationsItems.TELEPORT_STAFF,
+			new Identifier("pull"),
+			((itemStack, clientWorld, livingEntity, i) -> {
+				if (livingEntity == null) {
+					return 0f;
+				} else {
+					return livingEntity.getActiveItem() != itemStack ? 0f : TeleportStaffItem.getChargePercentage(itemStack, livingEntity.getItemUseTimeLeft());
+				}
+			})
+		);
+	}
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+		// register entity renderer for magic missile
+		EntityRendererRegistry.register(IntricationsEntities.MAGIC_MISSILE, MagicMissileEntityRenderer::new);
+
+	}
+}
---
+++ src/main/java/io/github/Tors_0/intrications/registry/IntricationsEntities.java
@@ -0,0 +1,30 @@
+package io.github.Tors_0.intrications.registry;
+
+import io.github.Tors_0.intrications.Intrications;
+import io.github.Tors_0.intrications.entity.MagicMissileEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.quiltmc.qsl.entity.api.QuiltEntityTypeBuilder;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public interface IntricationsEntities {
+	Map<EntityType<? extends Entity>, Identifier> ENTITIES = new LinkedHashMap<>();
+
+	//	public static EntityType<ModEntity> MOD_ENTITY = createEntity("mod_entity", QuiltEntityTypeBuilder.<ModEntity>create(SpawnGroup.MISC, ModEntity::new).setDimensions(EntityDimensions.fixed(0f, 0f)).maxChunkTrackingRange(128).build());
+	EntityType<MagicMissileEntity> MAGIC_MISSILE = createEntity("magic_missile", QuiltEntityTypeBuilder.<MagicMissileEntity>create(SpawnGroup.MISC, MagicMissileEntity::new).setDimensions(EntityDimensions.fixed(.5f,.5f)).maxChunkTrackingRange(128).build());
+
+	private static <T extends EntityType<? extends Entity>> T createEntity(String name, T entity) {
+		ENTITIES.put(entity, new Identifier(Intrications.ID, name));
+		return entity;
+	}
+
+	static void initialize() {
+		ENTITIES.keySet().forEach(entityType -> Registry.register(Registry.ENTITY_TYPE, ENTITIES.get(entityType), entityType));
+	}
+}
